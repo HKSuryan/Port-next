1cd6005745f37aab01d80ac47bc09fe5aaf24dd0
Harsh Kumar Suryan <89727538+HKSuryan@users.noreply.github.com>
2024-08-10 22:05:33 +0530

    apos

--- src/app/components/Contact.js
+++ src/app/components/Contact.js
@@ -6,7 +6,7 @@ const Contact = () => {
       <div className={styles.container}>
         <h2 className={styles.sectionTitle}>Get in Touch</h2>
         <p className={styles.subtitle}>
-          I’m always open to discussing new projects, creative ideas, or opportunities to be part of your visions.
+          I&apos;m always open to discussing new projects, creative ideas, or opportunities to be part of your visions.
         </p>
         <div className={styles.separ}>
         <div className={styles.contactContent}>
