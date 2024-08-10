cfec8ed4e6988f690f09d6a95385df9608dcd7e7
Harsh Kumar Suryan <89727538+HKSuryan@users.noreply.github.com>
2024-08-10 06:25:51 +0530

    frame

--- src/app/page.js
+++ src/app/page.js
@@ -13,7 +13,7 @@ export default function HomePage() {
   return (
     <>
       
-      <AnimationWrapper>
+      
         
 
          <Home/>
@@ -25,7 +25,7 @@ export default function HomePage() {
           <Contact/>
   
         
-      </AnimationWrapper>
+  
     </>
   );
 }
