e6f7bc86851ccb88d4994d9302a0fd650e42d644
Harsh Kumar Suryan <89727538+HKSuryan@users.noreply.github.com>
2024-09-14 00:14:59 +0530

    Added Better Sidebar

---
+++ src/app/components/Sidebar.jsx
@@ -0,0 +1,72 @@
+"use client";
+import { useEffect, useState } from "react";
+import styles from "../styles/Sidebar.module.css";
+import Link from "next/link";
+
+const Sidebar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [darkMode, setDarkMode] = useState(false);
+  const [theme, setTheme] = useState("light");
+
+  const toggleSidebar = () => {
+    setIsOpen(!isOpen);
+  };
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+    setTheme(!darkMode ? "light" : "dark");
+    document.documentElement.setAttribute("data-theme", theme);
+  };
+
+  // Sync dark mode with system preference
+  useEffect(() => {
+    const systemDarkMode = window.matchMedia(
+      "(prefers-color-scheme: dark)",
+    ).matches;
+    if (systemDarkMode) {
+      setDarkMode(true);
+      document.documentElement.setAttribute("data-theme", "dark");
+    }
+  }, []);
+
+  return (
+    <>
+      <div className={styles.logo}>
+        <a href="#home">@hksuryan</a>
+      </div>
+      <div className={styles.hamburger1} onClick={toggleSidebar}>
+        <span
+          className={`${styles.bar} ${isOpen ? styles.hamburgerOpen1 : ""}`}></span>
+        <span
+          className={`${styles.bar} ${isOpen ? styles.hamburgerOpen2 : ""}`}></span>
+        <span
+          className={`${styles.bar} ${isOpen ? styles.hamburgerOpen3 : ""}`}></span>
+      </div>
+      <aside className={`${styles.sidebar} ${isOpen ? styles.open : ""}`}>
+        <ul>
+          <li>
+            <Link href="#about">About</Link>
+          </li>
+          <li>
+            <Link href="#skills">Skills</Link>
+          </li>
+          <li>
+            <Link href="#project">Projects</Link>
+          </li>
+          <li>
+            <Link href="#experience">Experience</Link>
+          </li>
+          <li>
+            <Link href="#contact">Contact</Link>
+          </li>
+          <li>
+            <button className={styles.darkModeToggle} onClick={toggleDarkMode}>
+              {darkMode ? "☀️" : "🌙"}
+            </button>
+          </li>
+        </ul>
+      </aside>
+    </>
+  );
+};
+
+export default Sidebar;
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,15 +1,16 @@
-import './styles/globals.css';  // Import global styles
-import Navbar from './components/Navbar';
-import FooterSection from './components/FooterSection';
-
+import "./styles/globals.css"; // Import global styles
+import Navbar from "./components/Navbar";
+import FooterSection from "./components/FooterSection";
+import Sidebar from "./components/Sidebar";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
         <Navbar />
+        <Sidebar />
         {children} {/* This renders the content of the current page */}
-        <FooterSection/>
+        <FooterSection />
       </body>
     </html>
   );
